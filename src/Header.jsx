596fb8f767e209d136f42003f660af094263ac35
daragentle122 <dara.ly@student.cadt.edu.kh>
2025-03-16 17:16:28 +0700

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+
+function Header() {
+    return(
+        <header>
+            <h1>Blog</h1>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">Dogs</a></li>
+                    <li><a href="/contact">Cats</a></li>
+                </ul>
+            </nav>
+        </header>
+
+    );
+}
+
+export default Header;

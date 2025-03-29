c3caee73f1da5565265e58cdca99e40bd808af96
Virak Titya <viraktitya085@gmail.com>
2025-03-30 06:59:23 +0700

    Titya commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,15 @@
 
 function Header() {
     return(
-        <header>
-            <h1>Blog</h1>
-            <nav>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">Dogs</a></li>
-                    <li><a href="/contact">Cats</a></li>
-                </ul>
+        <header className="flex justify-around items-center bg-white text-gray-950 h-20">
+            <img class="w-60" src="/img/pets-boutique-high-resolution-logo.png" alt="" />
+            <nav className="flex gap-16 font-mono text-xl ">
+                <p><a href="/">Home</a></p>
+                <p><a href="/about">Dogs</a></p>
+                <p><a href="/contact">Cats</a></p>
             </nav>
         </header>
+        
 
     );
 }
